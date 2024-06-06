6f8aa6ba99051e3e08b7eca5a886ac5e6e50a7dd
Himanshu <108611598+himanshu-niet@users.noreply.github.com>
2024-06-07 03:54:47 +0530

    add

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express')
 const next = require('next')
 
 const port = 5000
-const dev = process.env.NODE_ENV !== 'production'
-
+const dev = false
 // creating the app either in production or dev mode 
 const app = next({ dev })
 const handle = app.getRequestHandler()
